2688d242a308f2247b6888d55bf95142924de08b
Michael Wong <michael.w228@gmail.com>
2021-05-27 18:58:39 -0400

    added styling and CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Header from './components/Header'
+
 
 function App() {
+
   return (
-    <div class="container">
-      <h1>Hello</h1>
+    <div className="container">
+      <Header/>
     </div>
   );
 }
+
 
 export default App;
