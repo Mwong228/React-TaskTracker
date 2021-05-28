05c5f45ed0425dac32b86928e92dee5d8d90007b
Michael Wong <michael.w228@gmail.com>
2021-05-27 21:38:08 -0400

    finished UI implementation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -25,6 +26,13 @@ function App() {
     }
   ])
 
+  //Add Task
+  function addTask(task){
+    const id = Math.floor(Math.random()*1000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   //Delete Task
   function deleteTask(id){
     setTasks(tasks.filter( task => task.id !== id))
@@ -38,8 +46,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask/>
+      <Header onAdd={()=> setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length >0 ?<Tasks tasks={tasks} 
       onDelete={deleteTask}
       onToggle={toggleReminder}
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 
-const AddTask = () => {
+const AddTask = ({onAdd}) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
 
+    function onSubmit(e){
+        e.preventDefault()
 
+        if(!text){
+            alert('Please Add a Task')
+            return
+        }
+        onAdd({text, day, reminder})
+        setText('')
+        setDay('')
+        setReminder(false)
+
+    }
     return (
-        <form className='add-form'>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className='form-control'>
                 <label>Task</label>
                 <input
@@ -28,11 +40,11 @@ const AddTask = () => {
                 <label>Reminder</label>
                 <input
                     type='checkbox'
-                    placeholder='Add Reminder'
+                    checked={reminder}
                     value={reminder}
                     onChange={(e) => setReminder(e.currentTarget.checked)} />
             </div>
-            <input type='Submit' value='Save Task' className='btn btn-block' />
+            <input type='submit' value='Save Task' className='btn btn-block' />
         </form>
     )
 }
